e4de8853de677c0150616aa396522918b58f649d
Daniel Soto Fuya <dsotofuya@gmail.com>
2023-11-02 11:43:30 -0500

    Init commit

---
+++ src/main/java/co/com/restaurante/ms/springmicroservicerestaurantexample/SpringMicroserviceRestaurantExampleApplication.java
@@ -0,0 +1,13 @@
+package co.com.restaurante.ms.springmicroservicerestaurantexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceRestaurantExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceRestaurantExampleApplication.class, args);
+	}
+
+}
