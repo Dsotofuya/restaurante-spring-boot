4b8016e59f2aa09124aece0e3fbb2fa8f33a472a
Daniel Soto Fuya <dsotofuya@gmail.com>
2023-11-04 08:59:33 -0500

    Update package-info.java
    Ajuste package-info, domain.service

--- src/main/java/co/com/restaurante/ms/springmicroservicerestaurantexample/domain/service/package-info.java
+++ src/main/java/co/com/restaurante/ms/springmicroservicerestaurantexample/domain/service/package-info.java
@@ -2,6 +2,10 @@
  * Contiene todas intefaces que tienen
  * las funcionalidades dentro de la logica del negocio
  *
+ * Adicionalmente se puede acoplar los servicios con @Service
+ * pero esto generaria dependencia con spring, para esto se realiza
+ * la configuración en la capa de aplicacion
+ *
  * @since 1.0
  * @author Daniel Soto Fuya
  * @version 1.0
